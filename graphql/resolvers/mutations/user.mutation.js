4243adfcd06f9fb5d93e79288816956897c0dec3
AyberkCakar <ayberkcakar35@gmail.com>
2020-02-09 19:35:59 +0300

    jwt added.

--- graphql/resolvers/mutations/user.mutation.js
+++ graphql/resolvers/mutations/user.mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+const token = require('../../../helpers/token');
+
 module.exports = {
   createUser: async (parent, { data: { username, password } }, { User }) => {
       const user = await User.findOne({ username });
@@ -6,9 +9,26 @@ module.exports = {
           throw new Error('User already exists.');
       }
 
-      return await  new User({
+      const newUser = await  new User({
          username,
          password
       }).save();
+
+      return { token: token.generate(newUser, '1h') }
+  },
+
+  singIn: async (parent, { data: { username, password } }, { User }) => {
+      const user = await User.findOne({ username });
+
+      if(!user){
+          throw new Error('User does not exists.');
+      }
+
+      const validPassword = await  bcrypt.compare(password,user.password);
+      if (!validPassword){
+          throw new Error('Wrong Password.');
+      }
+
+      return { token: token.generate(user, '1h') }
   }
 };
