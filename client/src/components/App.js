421a6c42aefdb4c18356b35b08b7774ad918554f
AyberkCakar <ayberkcakar35@gmail.com>
2020-02-10 00:44:32 +0300

    UI was integrated.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from 'react-router-dom';
+
+import Header from './Header';
+
+// pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Join from './pages/Join';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/join" component={Join} />
+            <Redirect to="/" />
+        </Switch>
+    </Router>
+);
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+      <div id="app">
+          <div className="container">
+                <Header/>
+
+                <Root/>
+          </div>
+      </div>
   );
 }
 
